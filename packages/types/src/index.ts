28a398e284ba69f9a43e6bff5041689878f6df99
Thiago <lucimara1422@gmail.com>
2023-06-22 13:54:46 -0300

    chore(import) rename

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,3 +1,3 @@
 export type { Maybe } from './Maybe';
 export type { DeepPartial } from './DeepPartial';
-export type { UserLoginMutationResult, UserRegisterMutationResult} from './UserTestType'
+export type { UserLoginMutationResult, UserRegisterMutationResult} from "../../../apps/server/test/InterfaceTest"
