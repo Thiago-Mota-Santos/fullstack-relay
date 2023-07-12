e0f6c1c30ea7c3c011d7cf7e540c6751ce2b45d6
Thiago <lucimara1422@gmail.com>
2023-07-12 15:24:40 -0300

    feat(relay config) add

---
+++ apps/docs/relay.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  src: './src',
+  language: 'typescript',
+  schema: './data/schema.gql',
+  //artifactDirectory: './src/__generated__',
+  exclude: [
+    '**/node_modules/**',
+    '**/__mocks__/**',
+    '**/__generated__/**',
+    '**/.next/**',
+  ],
+}
