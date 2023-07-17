dd608916a85df85d092ae67ded600065daf044be
Thiago <lucimara1422@gmail.com>
2023-07-17 16:38:17 -0300

    feat(appointment) component

---
+++ apps/web/src/components/NoAppointment.tsx
@@ -0,0 +1,16 @@
+import DialogButton from './DialogButton'
+
+function NoAppointment() {
+  return (
+    <div className="flex h-60 w-96 items-center justify-center rounded-2xl border-2 border-gray-600">
+      <div className="mb-4 flex flex-col items-center gap-2">
+        <h2 className="text-lg font-semibold text-gray-200">
+          Welcome! 🚀 Create your first Appointment
+        </h2>
+        <DialogButton />
+      </div>
+    </div>
+  )
+}
+
+export { NoAppointment }
