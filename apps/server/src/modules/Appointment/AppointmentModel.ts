35e8395733b5b9c98dd197aa4703fa3d94ab3e0d
Thiago <lucimara1422@gmail.com>
2023-07-18 18:07:37 -0300

    chore(_id) remove schema

--- apps/server/src/modules/Appointment/AppointmentModel.ts
+++ apps/server/src/modules/Appointment/AppointmentModel.ts
@@ -1,55 +1,51 @@
-import mongoose, { Document, Schema, Types } from "mongoose";
-import { Maybe } from "../../../../../packages/types/src/Maybe";
-
+import mongoose, { Document, Schema, Types } from 'mongoose'
+import { Maybe } from '../../../../../packages/types/src/Maybe'
 
 export interface Appointment {
-    clientName: string;
-    date: string;
-    hour: string;
-    graphicLocation: string;
-    service: "Internet" | "Xerox" | "Other" | "Banner";
-    _id: Types.ObjectId;
+  _id: Types.ObjectId
+  clientName: string
+  date: string
+  hour: string
+  graphicLocation: string
+  service: 'Internet' | 'Xerox' | 'Other' | 'Banner'
 }
 
-export type AppointmentDocument = Maybe<Appointment> & Document;
-
+export type AppointmentDocument = Maybe<Appointment> & Document
 
 const AppointmentSchema = new mongoose.Schema<Appointment>(
-    {
-       clientName: {
-        type: String,
-        required: true,
-        min: 3,
-        max: 30,
-        index: { unique: true }
-       },
-       date:{
-        type: String,
-        required: true,
-       },
-       hour: {
-        type: String,
-        required: true,
-       },
-       graphicLocation:{
-        type: String,
-        required: true,
-        min: 5,
-        max: 60
-       },
-       service:{
-        type: String,
-        required: true,
-       },
-       _id: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-       },
+  {
+    clientName: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 30,
+      index: { unique: true },
     },
-    {
-        collection: "Appointment"
-    }
+    date: {
+      type: String,
+      required: true,
+    },
+    hour: {
+      type: String,
+      required: true,
+    },
+    graphicLocation: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 60,
+    },
+    service: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    collection: 'Appointment',
+  },
 )
 
-
-export const AppointmentModel = mongoose.model<AppointmentDocument>("Appointment", AppointmentSchema)
+export const AppointmentModel = mongoose.model<AppointmentDocument>(
+  'Appointment',
+  AppointmentSchema,
+)
