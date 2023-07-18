f8361935059ccc21f2717892ccb11650d731f3ae
Thiago <lucimara1422@gmail.com>
2023-07-18 19:26:05 -0300

    .

---
+++ apps/server/src/auth.ts
@@ -0,0 +1,31 @@
+import { Context, ParameterizedContext } from "koa";
+import jwt from 'jsonwebtoken';
+
+import { config } from './config'
+
+import { UserDocument, UserModel } from "./modules/User/UserModel";
+
+
+export const getUser = async (token: Context) => {
+
+    if(!token) return { user: null };
+
+
+    try{
+        
+        const subToken = token.substring(6);
+        const decodedToken = jwt.verify(subToken, config.JWT_KEY);
+        const decodedId = decodedToken as { id: string };
+
+        const user = await UserModel.findOne({ _id: decodedId.id});
+
+        return { user };
+    }catch(err){
+        return { user: null };
+    }
+};
+
+export const generateJwtToken = (user: UserDocument) => {
+    return `JWT ${jwt.sign({ id: user._id }, config.JWT_KEY)}`;
+}
+
