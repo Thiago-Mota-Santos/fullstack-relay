7b9c19461158ecee375a34a59e37ed6a298a31fa
Thiago <lucimara1422@gmail.com>
2023-07-20 20:12:41 -0300

    feat(appointmentDelete) add test

---
+++ apps/server/src/modules/Appointment/__tests__/deleteAppointment.test.ts
@@ -0,0 +1,49 @@
+import { clearDatabaseAndRestartCounters } from '../../../../test/clearDatabase'
+import { mongooseConnection } from '../../../../test/mongooseConnection'
+import { mongooseDisconnect } from '../../../../test/mongooseDisconnect'
+import { createUser } from '../../User/fixture/createUser'
+import { getContext } from '../../../getContext'
+import { AppointmentDelete } from '../../../../test/InterfaceTest'
+import { schema } from '../../../schema/schema'
+import { getGraphqlResult } from '../../../../test/getGraphqlResult'
+import { createAppointment } from '../fixture/createAppointment'
+import { Types } from 'mongoose'
+
+beforeAll(mongooseConnection)
+beforeEach(clearDatabaseAndRestartCounters)
+afterAll(mongooseDisconnect)
+
+it('Should be delete appointment', async () => {
+  const user = await createUser()
+  const { _id } = await createAppointment({
+    clientName: 'Thiago',
+    date: '23-01-2022',
+    hour: '18:03',
+    graphicLocation: 'satelite-iris-3',
+    service: 'Banner',
+  })
+
+  const mutation = `
+    mutation appointment($appointmentId: String!) {
+      AppointmentDelete(input: { AppointmentId: $appointmentId }) {
+        success
+      }
+    }
+  `
+
+  const variableValues = {
+    appointmentId: _id.toString(),
+  }
+
+  const result = await getGraphqlResult<AppointmentDelete>({
+    schema,
+    source: mutation,
+    variableValues,
+    contextValue: getContext({ user }),
+  })
+
+  expect(result.errors).toBeUndefined()
+  expect(result.data?.AppointmentDelete.success).toBe(
+    'Appointment successfully deleted',
+  )
+})
