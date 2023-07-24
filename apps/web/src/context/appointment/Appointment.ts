15bf880ab96d74845a2fdd20c58ea114487df186
Thiago <lucimara1422@gmail.com>
2023-07-24 16:59:49 -0300

    feat(optmisticUpdate) add

--- apps/web/src/context/appointment/Appointment.ts
+++ apps/web/src/context/appointment/Appointment.ts
@@ -1,14 +1,46 @@
-import { graphql } from 'react-relay'
+import { ROOT_ID, RecordSourceSelectorProxy, graphql } from 'relay-runtime'
+import { connectionUpdater } from '../../utils/connectionUpdater'
 
-const Appointment = graphql`
+const AppointmentQuery = graphql`
   query AppointmentQuery {
     appointments {
       edges {
         node {
-          date
-          hour
+          id
           clientName
           service
+          date
+          hour
+          graphicLocation
+        }
+      }
+    }
+  }
+`
+
+const Appointment = graphql`
+  mutation AppointmentMutation(
+    $clientName: String!
+    $date: String!
+    $hour: String!
+    $graphicLocation: String!
+    $service: String!
+  ) {
+    appointmentRegisterMutation(
+      input: {
+        clientName: $clientName
+        date: $date
+        hour: $hour
+        graphicLocation: $graphicLocation
+        service: $service
+      }
+    ) {
+      appointmentEdge {
+        node {
+          clientName
+          service
+          date
+          hour
           graphicLocation
           id
         }
@@ -17,4 +49,18 @@ const Appointment = graphql`
   }
 `
 
-export { Appointment }
+const updater = (store: RecordSourceSelectorProxy) => {
+  const newEdge = store
+    .getRootField('appointmentRegisterMutation')
+    .getLinkedRecord('appointmentEdge')
+
+  connectionUpdater({
+    store,
+    parentId: ROOT_ID,
+    connectionName: 'AppointmentList_appointments',
+    edge: newEdge,
+    before: true,
+  })
+}
+
+export { Appointment, updater, AppointmentQuery }
