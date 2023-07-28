669371ef454edfc99ee45ab069f93f8ad2f73255
Thiago <lucimara1422@gmail.com>
2023-07-28 13:25:19 -0300

    chore(remove alias @)

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['pagesQuery.graphql.ts'],
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 25 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 2,
+  },
 }
 
 module.exports = nextConfig
