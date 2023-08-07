21167dd3d25361c94749ce1bb06b22f28f3d4c10
Thiago <lucimara1422@gmail.com>
2023-08-07 11:17:55 -0300

    feat(replace) auth context

--- apps/web/src/context/AuthContext.tsx
+++ apps/web/src/context/AuthContext.tsx
@@ -1,39 +1,45 @@
-import React, { useState, useMemo, useCallback } from 'react'
+import { Maybe } from '@/interfaces/Maybe';
+import { destroyCookie, parseCookies, setCookie } from 'nookies';
 
-import { delAuthToken, getAuthToken, setAuthToken } from '../utils/getToken'
-import { Maybe } from '@/interfaces/Maybe'
+import React, { useState, useMemo, useCallback } from 'react';
 
 interface AuthContextValue {
-  token: Maybe<string>
-  signIn: (token: Maybe<string>) => void
-  signout: () => void
+  token: Maybe<string>;
+  signin: (token: Maybe<string>) => void;
+  signout: () => void;
 }
 
-export const AuthContext = React.createContext({} as AuthContextValue)
+const AUTH_COOKIE = 'graphic-token'
+export const AuthContext = React.createContext({} as AuthContextValue);
+
+const getAuthToken = (): Maybe<string> => {
+  const { AUTH_COOKIE: token } = parseCookies()
+  return token
+}
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [userToken, setUserToken] = useState<AuthContextValue['token']>(() =>
-    getAuthToken(),
-  )
-
-  const signIn = useCallback<AuthContextValue['signIn']>((token) => {
-    setAuthToken(token || '')
-    setUserToken(token)
-  }, [])
+  const [userToken, setUserToken] = useState<AuthContextValue['token']>(() => getAuthToken());
+  const signin = useCallback<AuthContextValue['signin']>((token) => {
+    setCookie(undefined, AUTH_COOKIE, token, {
+      maxAge: 3600 * 24 * 7,
+      path: '/',
+    })
+    setUserToken(token);
+  }, []);
 
   const signout = useCallback<AuthContextValue['signout']>(() => {
-    setUserToken(null)
-    delAuthToken()
-  }, [])
+    setUserToken(null);
+    destroyCookie(undefined, AUTH_COOKIE)
+  }, []);
 
   const value = useMemo<AuthContextValue>(
     () => ({
       token: userToken,
-      signIn,
+      signin,
       signout,
     }),
-    [userToken, signIn, signout],
-  )
+    [userToken, signin, signout],
+  );
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
-}
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
