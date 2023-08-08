451461b5f4e92c121e0b997bef1dfa382b386c91
Thiago <lucimara1422@gmail.com>
2023-08-08 17:17:07 -0300

    test(prod) token

--- apps/server/src/auth.ts
+++ apps/server/src/auth.ts
@@ -1,3 +1,4 @@
+import { VercelRequest } from '@vercel/node'
 import jwt from 'jsonwebtoken'
 
 import { UserDocument, UserModel } from './modules/User/UserModel'
@@ -7,23 +8,41 @@ import { ParameterizedContext } from 'koa'
 
 const JWT_KEY = process.env.JWT_KEY as string
 const getUser = async (
-  ctx: ParameterizedContext,
+  ctx: VercelRequest,
+  // request: VercelRequest
 ): Promise<{ user: Maybe<UserDocument> }> => {
-  const token = ctx.cookies.get('_vercel_jwt')
+  // const token = ctx.cookies.get('_vercel_jwt')
 
-  try {
-    if (!token) return { user: null }
+  const cookieHeader = ctx.headers['cookie']
+  const cookies: { [key: string]: string } = {}
+  if (cookieHeader) {
+    cookieHeader.split(';').forEach((cookie) => {
+      const parts = cookie.split('=')
+      const key = parts[0].trim()
+      const value = parts[1].trim()
+      cookies[key] = value
+    })
+  }
+  const { x_authorization } = ctx.headers
+  const token = cookies['_vercel_jwt']
+  console.log('AUTH : ' + x_authorization)
 
-    const subToken = token.substring(6)
-    const decodedToken = jwt.verify(subToken, JWT_KEY)
-    const decodedId = decodedToken as { id: string }
+  if (x_authorization === process.env.JWT_TOKEN) {
+    console.log(x_authorization)
+    try {
+      if (!token) return { user: null }
 
-    debugConsole(decodedId)
+      const subToken = token.substring(6)
+      const decodedToken = jwt.verify(subToken, JWT_KEY)
+      const decodedId = decodedToken as { id: string }
 
-    const user = await UserModel.findOne({ _id: decodedId.id })
-    return { user }
-  } catch (err) {
-    return { user: null }
+      debugConsole(decodedId)
+
+      const user = await UserModel.findOne({ _id: decodedId.id })
+      return { user }
+    } catch (err) {
+      return { user: null }
+    }
   }
 }
 
